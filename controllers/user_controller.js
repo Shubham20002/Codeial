cbdd9da26d91ac167ec7a430638f7df28560a9fb
shubham <shubham992284@gmail.com>
2023-05-24 17:40:57 +0530

    user controller added

---
+++ controllers/user_controller.js
@@ -0,0 +1,3 @@
+module.exports.profile=function(req,res){
+   return res.end('<h1>we are on user profile page</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 
+
 const router=express.Router();
 const homecontroller=require('../controllers/home_controller');
 
 router.get('/',homecontroller.home);
+
+router.use('/user',require('./user'));
 
 console.log("router loaded");
 module.exports=router;
